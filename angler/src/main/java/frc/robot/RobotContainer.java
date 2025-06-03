b6bd68c764babaac2306ae12be5b79d603c15b81
Yoda-Flash <kaitlyn.zee@gmail.com>
2025-06-03 22:16:33 +0800

    Added arcade arm command and the robot container implementation, simulated and confirmed to be correct, I think

--- angler/src/main/java/frc/robot/RobotContainer.java
+++ angler/src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,29 @@
 
 package frc.robot;
 
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.arm.ArcadeArm;
+import frc.robot.constants.JoystickConstants;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.ArmIOTalonFX;
 
 public class RobotContainer {
+
+  private final Joystick m_driverJoystick = new Joystick(JoystickConstants.kDriverJoystickPortID);
+
+  private final ArmIOTalonFX m_armIO = new ArmIOTalonFX();
+  private final Arm m_arm = new Arm(m_armIO);
+  private final ArcadeArm m_arcadeArm = new ArcadeArm(m_arm, m_driverJoystick);
+
   public RobotContainer() {
+    m_arm.setDefaultCommand(m_arcadeArm);
+
+    SendableRegistry.add(m_arm.getSendable(), "Arm");
+    Shuffleboard.getTab("SmartDashboard").add(m_arm.getSendable()).withWidget("Arm");
+
     configureBindings();
   }
 
